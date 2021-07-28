e8096cbdaad2ca6aeecdff98d3f28b835d25b8fb
Dario Vitorte <vitortedario@gmail.com>
2021-07-28 15:53:03 -0700

    added more commands

--- app1.js
+++ app1.js
@@ -1,2 +1,4 @@
-let favFood = prompt('What is your favorite food?')
-let location = prompt('Where are you from?')
+console.log("helloworld")
+let Ulocation = prompt('Where are you from?')
+let Uname = prompt('What is your name?')
+document.write('<p>Hi ' + Uname + ' from ' + Ulocation + ' </p>' )
