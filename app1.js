8d11078204bd023aade0a1813e78eba312d8038c
Dario Vitorte <vitortedario@gmail.com>
2021-07-30 14:27:21 -0700

    typo

--- app1.js
+++ app1.js
@@ -2,3 +2,10 @@ console.log("helloworld")
 let Ulocation = prompt('Where are you from?')
 let Uname = prompt('What is your name?')
 document.write('<p>Hi ' + Uname + ' from ' + Ulocation + ' </p>' )
+let howMP = prompt('How many plates will you need to eat')
+function plates() {
+    var plateN = howMP
+    for (i=0; i < plateN; i++){
+        document.write('<img src="https://www.ikea.com/us/en/images/products/oftast-side-plate-white__0713530_pe729595_s5.jpg?f=s">')
+    }
+}
